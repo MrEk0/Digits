a68965806f23472d706873ca828d9fc69af5197f
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-03-13 10:27:07 +0300

    spawn objects

---
+++ Digits/Assets/DropSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropSystem : MonoBehaviour
+{
+    [SerializeField] GameObject objectPrefab;
+    [SerializeField] float timeBetweenSpawns = 10f;
+
+    float timeSinceLastSpawn = Mathf.Infinity;
+    List<Vector3> tilePositions;
+    List<int> takenTiles;
+
+    private void Awake()
+    {
+        tilePositions = new List<Vector3>();
+        takenTiles = new List<int>();
+
+        foreach (Transform child in transform)
+        {
+            tilePositions.Add(child.localPosition);
+        }
+    }
+
+    private void Update()
+    {
+        if(timeSinceLastSpawn>timeBetweenSpawns)
+        {
+            int tileIndex = Random.Range(0, tilePositions.Count);
+        
+            if (takenTiles.Contains(tileIndex))
+                return;
+
+            takenTiles.Add(tileIndex);
+            GameObject obj = Instantiate(objectPrefab, transform);
+            obj.transform.localPosition = tilePositions[tileIndex];
+            timeSinceLastSpawn = 0;
+        }
+        timeSinceLastSpawn += Time.deltaTime;
+    }
+}
