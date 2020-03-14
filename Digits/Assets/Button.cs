e91af1b769732505ce25fcd84ef06735311c16a5
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-03-14 22:38:48 +0300

    scroll panel

---
+++ Digits/Assets/Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI numberText;
+    [SerializeField] TextMeshProUGUI priceText;
+
+    private float numberOfPurchases=0;
+    private float price;
+
+    public float Number { get; set; }
+
+    private void Start()
+    {
+        FormButtonDescription();
+    }
+
+    private void FormButtonDescription()
+    {
+        numberText.text = Number.ToString();
+
+        SetPrice();
+    }
+
+    public void MakeAPurchase()
+    {
+        numberOfPurchases++;
+        SetPrice();
+    }
+
+    private void SetPrice()
+    {
+        price = Number * 100 * Mathf.Pow((numberOfPurchases + 1), 2);
+        priceText.text = "Buy: "+price.ToString();
+    }
+}
---
+++ Digits/Assets/ScrollPanel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollPanel : MonoBehaviour
+{
+    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] float numberOfButtons = 100;
+
+    RectTransform rectTransform;
+    VerticalLayoutGroup verticalLayout;
+
+    private void Start()
+    {
+        FormScrollPanel();
+        SetScrollHeight();
+    }
+
+    private void SetScrollHeight()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        verticalLayout = GetComponent<VerticalLayoutGroup>();
+
+        float spacing = verticalLayout.spacing;
+        float topOffset = verticalLayout.padding.top;
+        float bottomOffset = verticalLayout.padding.bottom;
+        float currentHeight = rectTransform.rect.height;
+        float buttonHeight = buttonPrefab.GetComponent<RectTransform>().rect.height;
+
+        float height = ((buttonHeight + spacing) * numberOfButtons + topOffset + bottomOffset )- currentHeight;
+        rectTransform.sizeDelta = new Vector2(0, height);
+        rectTransform.localPosition = new Vector3(0, rectTransform.rect.yMin);
+    }
+
+    private void FormScrollPanel()
+    {
+        for (int i = 0; i < numberOfButtons; i++)
+        {
+            GameObject button = Instantiate(buttonPrefab, transform);
+            button.GetComponent<Button>().Number = i + 1;
+        }
+    }
+}
